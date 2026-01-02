735146eb1de9bbb88b868edaa95e15a4889e5b1b
Arsharaj Chauhan <arsharajchauhan343@gmail.com>
2026-01-02 12:33:30 +0530

    problem 66 : plus one

---
+++ go/66_plus_one.go
@@ -0,0 +1,21 @@
+func plusOne(digits []int) []int {
+	n := len(digits) - 1
+
+	if digits[n] != 9 {
+		digits[n]++
+		return digits
+	}
+
+	for n >= 0 && digits[n] == 9 {
+		digits[n] = 0
+		n--
+	}
+
+	if n >= 0 {
+		digits[n]++
+	} else {
+		digits = append([]int{1}, digits...)
+	}
+
+	return digits
+}
